acf8e37c9875ed3be9151287ab8ae098d2a2d3c7
Jakub Brzazgacz <jakubpr19@gmail.com>
2025-04-19 11:47:20 +0200

    No pipeline model

--- rtl-model/quadra.sv
+++ rtl-model/quadra.sv
@@ -38,63 +38,29 @@ module quadra
 
     square u_square(
         .x2(x2),
-        .sq(sq_nxt)
+        .sq(sq)
     );    
 
     lut u_lut(
         .x1(x1),
-        .a(a_nxt),
-        .b(b_nxt),
-        .c(c_nxt)
+        .a(a),
+        .b(b),
+        .c(c)
     );
 
-    // Pipeline stage 1 ff
-    always_ff@(posedge clk) begin
-        if (!rst_b) begin
-            sq <= '0;
-            a <= '0;
-            b <= '0;
-            c <= '0;
-            x2_internal <= '0;
-        end
-        else begin
-            sq <= sq_nxt;
-            a <= a_nxt;
-            b <= b_nxt;
-            c <= c_nxt;
-            x2_internal <= x2;
-        end
-    end
-
     always_comb begin
-        t0_nxt = signed'(a >> 1);
-        t1_full = b * signed'(x2_internal >> X2_SHIFT); // shift right 6 bits for proper interpretation -> * 2^(-6)
-        t1_nxt = signed'(t1_full[T1_FULL_W-1:T1_FULL_W-T1_W]);
+        t0 = signed'(a >> 1);
+        t1_full = b * signed'(x2 >> X2_SHIFT); // shift right 6 bits for proper interpretation -> * 2^(-6)
+        t1 = signed'(t1_full[T1_FULL_W-1:T1_FULL_W-T1_W]);
         t2_full = c * signed'(sq >> X2_SHIFT*2);        // shift right 12 bits for proper alignment -> * 2^(-12)
-        t2_nxt = signed'(t2_full[T2_FULL_W-1:T2_FULL_W-T2_W]);
-    end
+        t2 = signed'(t2_full[T2_FULL_W-1:T2_FULL_W-T2_W]);
 
-    // Pipeline stage 2 ff
-    always_ff@(posedge clk) begin
-        if (!rst_b) begin
-            t0 <= '0;
-            t1 <= '0;
-            t2 <= '0;
-        end
-        else begin
-            t0 <= t0_nxt;
-            t1 <= t1_nxt;
-            t2 <= t2_nxt;
-        end
-    end
-
-    always_comb begin
         s_full = t0 + t1 + t2;
         sum = signed'(s_full[S_FULL_W-3:0]);
+
         y_nxt = signed'(sum[S_W-1:R_F]);
     end
 
-    // Pipeline stage 3 ff
     always_ff@(posedge clk) begin
         if (!rst_b) begin
             y <= '0;
